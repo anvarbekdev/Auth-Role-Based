2f9b6627b63c3611c893c0b154696b5a8aaa11fe
Anvarbek <anvarbekxaydarov777@gmail.com>
2023-09-14 19:33:39 +0500

    first commit

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,68 @@
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { GithubProfile } from "next-auth/providers/github";
+
+export const options: NextAuthOptions = {
+  providers: [
+    // GitHubProvider({
+    //     profile(profile: GithubProfile) {
+    //         //console.log(profile)
+    //         return {
+    //             ...profile,
+    //             role: profile.role ?? "user",
+    //             id: profile.id.toString(),
+    //             image: profile.avatar_url,
+    //         }
+    //     },
+    //     clientId: process.env.GITHUB_ID as string,
+    //     clientSecret: process.env.GITHUB_SECRET as string,
+    // }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "your-cool-username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "your-awesome-password",
+        },
+      },
+      async authorize(credentials) {
+        // This is where you need to retrieve user data
+        // to verify with credentials
+        // Docs: https://next-auth.js.org/configuration/providers/credentials
+        const user = {
+          id: "42",
+          name: "test",
+          password: "test",
+          role: "admin",
+        };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    jwt({ token, user }) {
+      console.log("token", token);
+      if (user) token.role = user.role;
+      return token;
+    },
+    session({ session, token }) {
+      session.user.role = token.role;
+      return session;
+    },
+  },
+};
